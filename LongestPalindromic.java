ddcd363834f4c6a9f007b9e42c42aa6c078ff862
Anurag Bhattacharjee <hamsen3880@outlook.com>
2025-09-05 20:16:53 +0530

    Update LongestPalindromic.java

--- LongestPalindromic.java
+++ LongestPalindromic.java
@@ -1,21 +1,20 @@
-import java.util.*;
+public class LPSRecursive {
+    public static int lps(String s, int i, int j) {
+        if (i > j) return 0;     
+        if (i == j) return 1;   
 
-public class LongestPalindromic {
-   public static int lps(String s, int i, int j) {
-    if (i > j) return 0;
-    if (i == j) return 1;
+        if (s.charAt(i) == s.charAt(j)) {
+            return 2 + lps(s, i + 1, j - 1);
+        } 
 
-    if (s.charAt(i) == s.charAt(j)) {
-        return 2 + lps(s, i+1, j-1);
-    } else {
-        return Math.max(lps(s, i+1, j), lps(s, i, j-1));
-    }
-}
+        else {
+            return Math.max(lps(s, i + 1, j), lps(s, i, j - 1));
+        }
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        String s = sc.nextLine();
-        System.out.println(LongestPalindromicSubsequence(s));
-        sc.close();
+        String s = "bbbab";
+        int result = lps(s, 0, s.length() - 1);
+        System.out.println("Longest Palindromic Subsequence length = " + result);
     }
 }
