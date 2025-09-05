a0ffe23d94078ada71a7d0b60210be94e15537b5
Anurag Bhattacharjee <hamsen3880@outlook.com>
2025-09-05 20:14:31 +0530

    Update LongestPalindromic.java

--- LongestPalindromic.java
+++ LongestPalindromic.java
@@ -1,34 +1,16 @@
 import java.util.*;
 
 public class LongestPalindromic {
-    public static int lcs(String s1, String s2, int i, int j, int[][] memo) {
-        if (i == 0 || j == 0) {
-            return 0;
-        }
-        if (memo[i][j] != -1) {
-            return memo[i][j];
-        }
+   public static int lps(String s, int i, int j) {
+    if (i > j) return 0;
+    if (i == j) return 1;
 
-        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-            memo[i][j] = 1 + lcs(s1, s2, i - 1, j - 1, memo);
-        }
-        else {
-            memo[i][j] = Math.max(lcs(s1, s2, i - 1, j, memo), lcs(s1, s2, i, j - 1, memo));
-        }
-
-        return memo[i][j];
-    }
-
-    public static int LongestPalindromicSubsequence(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
-        int n = s.length();
-        int[][] memo = new int[n + 1][n + 1];
-        for (int i = 0; i < memo.length; i++) {
-            Arrays.fill(memo[i], -1);
-        }
-
-        return lcs(s, rev, n, n, memo);
-    }
+    if (s.charAt(i) == s.charAt(j)) {
+        return 2 + lps(s, i+1, j-1);
+    } else {
+        return Math.max(lps(s, i+1, j), lps(s, i, j-1));
+    }
+}
 
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
